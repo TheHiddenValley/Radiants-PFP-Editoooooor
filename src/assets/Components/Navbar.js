5344b95bbf2a92324ba914967796cbb43bc41a57
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-08 02:06:30 +0800

    Update Navbar.js

--- src/assets/Components/Navbar.js
+++ src/assets/Components/Navbar.js
@@ -4,13 +4,16 @@ import logo from "../Images/logo.png";
 function Navbar(props) {
     return (
         <nav className="navbar bg-black">
-            <div className="container-fluid">
+            <div className="container-fluid d-flex justify-content-center">
                 <a className="navbar-brand" href="/" alt="preview">
                     <img
                         src={logo}
                         alt="Logo"
-                        width="auto"
-                        height="22vh"
+                        style={{
+                            maxWidth: "80vw",
+                            minWidth: "40vw",
+                            height: "auto",
+                        }}
                         className="d-inline-block align-text-top"
                     />
                     &nbsp; Background Remover
