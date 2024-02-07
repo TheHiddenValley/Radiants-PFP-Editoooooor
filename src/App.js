3f90640dbfe665af521b04369263a1e39ff0dd5d
TheHiddenValley <zackrikk@gmail.com>
2024-02-07 17:05:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./assets/Components/Navbar";
+import Main from "./assets/Components/Main";
+import Footer from "./assets/Components/Footer";
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
